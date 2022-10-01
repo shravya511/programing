691c3440e7fdef3b003008406e5cee7a5486e1f5
satvik <satvikrshetty423@gmail.com>
2022-10-01 23:03:06 +0530

    Exercise 2 completed

---
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-4- Randomisation and Python Lists/Exercise 2 - Banker Roulette.py
@@ -0,0 +1,21 @@
+# Instructions
+# You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+# Important: You are not allowed to use the choice() function.
+# Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
+# When you run the code, just use a random number as the seed. e.g. 67346 It doesn't matter what you chose, it's only for our testing code to check your work.
+# Example Input
+# Angela, Ben, Jenny, Michael, Chloe
+# Note: notice that there is a space between the comma and the next name.
+# Example Output
+# Michael is going to buy the meal today!
+
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+number_of_items=len(names)
+num=random.randint(0,number_of_items-1)
+print(f"{names[num]} is going to buy the meal today!")
