6eba5821a991662b97a5e4da3c9913bacf9fac25
satvik <satvikrshetty423@gmail.com>
2022-10-01 11:55:45 +0530

    answer

--- python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/Exercise 2 - BMI Calculator.py
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/Exercise 2 - BMI Calculator.py
@@ -16,3 +16,10 @@
 # 80 ÷ (1.75 x 1.75) = 26.122448979591837
 # 26
 
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+print(int(int(weight)/(float(height)*float(height))))
