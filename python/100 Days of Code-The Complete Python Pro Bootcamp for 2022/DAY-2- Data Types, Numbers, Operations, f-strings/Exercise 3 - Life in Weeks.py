ef6f1345ba2a18942d7e0615b200ed50dc331906
satvik <satvikrshetty423@gmail.com>
2022-10-01 19:16:18 +0530

    minor changes

--- python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/Exercise 3 - Life in Weeks.py
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/Exercise 3 - Life in Weeks.py
@@ -21,15 +21,15 @@
 # 🚨 Don't change the code below 👇
 age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
-m=1080
-w=4680
-d=32850
+m=1080    #--
+w=4680    #  |-- this is done 'cuz 90 years has 1080 month, 4680 weeks and 32850 days
+d=32850  #--
 #Write your code below this line 👇
 a=int(age)
-m1=a*12
-w1=a*52
-d1=a*365
-m2=m-m1
-w2=w-w1
-d2=d-d1
+m1=a*12     #---                                                                 remaining_years = 90 - a
+w1=a*52     #   |                                                                 d2 = remaining_years * 365
+d1=a*365   #   |----this code can be reduced like this -->  w2 = remaining_years * 52
+m2=m-m1   #   |                                                                  m2 = remaining_years * 12
+w2=w-w1    #   |
+d2=d-d1     #---
 print(f"You have {d2} days, {w2} weeks, and {m2} months left.")
