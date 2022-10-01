c8f47dc6c9c2b76077f6e58d3c77111fb6ca0dd3
satvik <satvikrshetty423@gmail.com>
2022-10-01 19:16:41 +0530

    added new notes

--- python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/notes.py
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/notes.py
@@ -1,4 +1,4 @@
-print("DAY-1")
+print("DAY-2")
 print("-----------------------------------------------------------------------------------------------------------")
 print("hello"[0])
 print(type(12))
@@ -6,3 +6,8 @@ char_num=str(len(input("Whats your name? ")))
 print("Your name has " + char_num + " charecters") 
 a=type(123)
 print(a)
+print(1_234_567_889)    #over here '_' is not considered 
+print(2**3)     # here '**' means power operator
+print(round(8/3,3))     # here round() is used to round the floating point number. this function takes 2 inputs, one is the operation and another is precision points.
+print(8//3) # '//' this is called flow dividion, used to convert the result to int automatically.
+print(f"Using f-string {a} ---- {char_num}")    # f-string is used to automatically concatinate different data types in output screen.
