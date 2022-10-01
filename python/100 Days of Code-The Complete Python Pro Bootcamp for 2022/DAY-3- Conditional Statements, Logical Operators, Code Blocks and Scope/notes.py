20441b7282519bdff91e32166d6810b917dee7eb
satvik <satvikrshetty423@gmail.com>
2022-10-01 21:21:44 +0530

    added notes

---
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-3- Conditional Statements, Logical Operators, Code Blocks and Scope/notes.py
@@ -0,0 +1,42 @@
+print("DAY-2")
+print("-----------------------------------------------------------------------------------------------------------")
+print("Using if statement")
+a = input("Value of a is: ")
+b = input("Value of b is: ")
+if a == b:
+    print("A and B are equal")
+else:
+    print("A and B are not equal")
+
+print("Using nested if statement")
+if a == b:
+    print("A and B are equal")
+else:
+    if a > b:
+        print("A is greater than B")
+    else:
+        print("B is greater than A")
+
+print("Using elif statement")
+if a == b:
+    print("A and B are equal")
+elif a > b:
+    print("A is greater than B")
+elif a < b:
+    print("B is greater than A")
+
+print("Using .lower()")
+a=input("Enter capital letter: ")
+print(a.lower())    # .lower() used to convert capital letters to lower case letters
+
+print("Using .count()")
+print(f"Counting number of 'A' in capital letters: {a.count('A')}")     # .count() is used returns the number of elements with the specified value.
+
+print('''    
+hey
+this 
+is
+multi 
+line
+string
+''')    # we use ''' <- this to make multi-line string
