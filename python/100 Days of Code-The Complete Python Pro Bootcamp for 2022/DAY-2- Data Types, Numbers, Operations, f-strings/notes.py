885e742e586fd420f0d6f9d6b1b32fd6bfc72118
satvik <satvikrshetty423@gmail.com>
2022-10-01 19:42:42 +0530

    added new notes

--- python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/notes.py
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-2- Data Types, Numbers, Operations, f-strings/notes.py
@@ -9,5 +9,6 @@ print(a)
 print(1_234_567_889)    #over here '_' is not considered 
 print(2**3)     # here '**' means power operator
 print(round(8/3,3))     # here round() is used to round the floating point number. this function takes 2 inputs, one is the operation and another is precision points.
+print("using .format() and output the answer of 8/3 ->"+ "{:.2f}".format(8/3))
 print(8//3) # '//' this is called flow dividion, used to convert the result to int automatically.
 print(f"Using f-string {a} ---- {char_num}")    # f-string is used to automatically concatinate different data types in output screen.
