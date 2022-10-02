6416b44865bd4c09af90c3531a77617a8cd3fb68
satvik <satvikrshetty423@gmail.com>
2022-10-02 07:03:03 +0530

    updated exercise 3

--- python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-4- Randomisation and Python Lists/Exercise 3 - Treasure Map.py
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-4- Randomisation and Python Lists/Exercise 3 - Treasure Map.py
@@ -32,7 +32,7 @@ row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
+position = input("Where do you want to put the treasure?\nEnter two digit number such that ones place hold row value and tens place holds coloumn value: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
