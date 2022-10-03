25cd6bf3a672984fd677fc436ae27c4253236e20
satvik <satvikrshetty423@gmail.com>
2022-10-03 09:51:10 +0530

    Added notes

---
+++ python/100 Days of Code-The Complete Python Pro Bootcamp for 2022/DAY-8- Functions With Inputs/notes.py
@@ -0,0 +1,11 @@
+print("DAY-8")
+print("-----------------------------------------------------------------------------------------------------------")
+def my_function():
+    print("hello")
+def my_function1(a):
+    print(a)
+def my_function2(a,b,c):
+    print(f"{a} {b} {c}")
+my_function()
+my_function1("nooo")
+my_function2(c =3,b = 2,a = 1)
